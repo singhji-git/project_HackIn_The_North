3d21891508b51de7ed5e6f4e91f8053e2c1b4caa
saurav kumar <sauarvk048@gmail.com>
2017-03-24 07:04:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,42 +27,27 @@ def webhook():
     r.headers['Content-Type'] = 'application/json'
     return r
 
-
-
 def makeWebhookResult(req):
-    x = req.get("result").get("action")
-#     if  x != "current.plan" and  (x != "current.planchange" and x != "bill.enquiry"):
-#         return {}
-    
+    if req.get("result").get("action") != "no.item":
+        return {}
     result = req.get("result")
     parameters = result.get("parameters")
-    speech="hey"
-    message=""
-    plan = {'9155465072':"Free Roaming", '9572390164':"Free Calling", '919973212':"Free 1GB Data", '9973617212':"30p/min", '91998870950':"Free Videocalling"}
-    bill = {'9155465072': "100" , '9572390164': "200" , '919973212': "300" , '9973617212': "350.45" , '91998870950': "345.23" }
-    subscription = {'9155465072': "callerTuneActivated" , '9572390164': "none" , '919973212': "callerTuneActivated" , '9973617212': "none" , '91998870950': "callerTuneActivated" }
     
+
     
-    if req.get("result").get("action") == "no.item":
-        speech = "no more items to be addded" 
-            
-        
+
+    speech = "hey our Ngos will be contacting you soon."
 
     print("Response:")
     print(speech)
 
     return {
-        
         "speech": speech,
-
         "displayText": speech,
-
         #"data": {},
         # "contextOut": [],
-        "source": "apiai-onlinestore-shipping"
+        "source": "apiai-HackInTheNorth"
     }
-
-
 
 
 if __name__ == '__main__':
