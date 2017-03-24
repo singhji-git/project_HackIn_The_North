ce14c790e28817f92b9eee09e81982b0f7283d2a
saurav kumar <sauarvk048@gmail.com>
2017-03-24 06:56:00 +0530

    no.items action sample handled

--- app.py
+++ app.py
@@ -42,52 +42,9 @@ def makeWebhookResult(req):
     bill = {'9155465072': "100" , '9572390164': "200" , '919973212': "300" , '9973617212': "350.45" , '91998870950': "345.23" }
     subscription = {'9155465072': "callerTuneActivated" , '9572390164': "none" , '919973212': "callerTuneActivated" , '9973617212': "none" , '91998870950': "callerTuneActivated" }
     
-    number = parameters.get("Phonenumber")
-    if (plan.get(number) == None and number != None):
-        speech = "Your number doesnot exist in database, please try with another one :) " 
-    elif req.get("result").get("action") == "current.plan":
-        number = parameters.get("Phonenumber")
-        comp= parameters.get("Company")
-        contexts1=req.get("result").get("contexts")
-        contexts2 = contexts1[0].get("name")
-        #if contexts1[0].name == "test" or contexts1[0].name=="":
-        x = contexts1[0].get("parameters").get("prephone")
-        if x is None or x != number :
-            speech = "The current plan of the user with phone no. " + number + " is " + str(plan[number]) + "."
-        else :
-            y = contexts1[0].get("parameters").get("Plan")
-            speech = "The current plan of the user with phone no. " + number + " is " +  y + "."
-    elif req.get("result").get("action") == "current.planchange":
-        number = parameters.get("Phonenumber")
-        newplan = parameters.get("Plan")
-        prevplan = plan[number]
-        plan[number] = newplan
-        speech = "The plan is changed from  " + prevplan + " to " + str(plan[number]) + " for the " + number + "."
-    elif req.get("result").get("action") == "bill.enquiry":
-        number = parameters.get("Phonenumber")
-        speech = "The bill for   " + number + " is " + str(bill[number]) + "."
-        
-        
-        
-        
-    elif req.get("result").get("action") == "bill.highcomplain" :
-        number = parameters.get("Phonenumber")
-        subscription1 = subscription[number]
-        if subscription1 == "none" :
-            speech = "you phone no. " + number + " is not subscribed for any featues.."
-        else :
-            speech = "you phone no. " + number + " is  subscribed to " + subscription1 + "and do you want to unsubscribe this subscription ?"
     
-    elif req.get("result").get("action") == "subscription.yes" :
-        number = parameters.get("Phonenumber")
-        speech = "you have been unsubscribed for the feature" + subscription[number] +"."
-        subscription[number] = "none"
-    
-    elif req.get("result").get("action") == "input.unknown" :
-        speech = "I Missed it! can you say that again...."
-#         with open('message.json') as json_data:
-#             message = json.loads(json_data)
-#             json_data.close()
+    if req.get("result").get("action") == "no.item":
+        speech = "no more items to be addded" 
             
         
 
